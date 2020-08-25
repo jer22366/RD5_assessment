1aeb49304242bef84f4c454f2b17be0d1e9f98fb
CY0290 <65933458+locklen860118@users.noreply.github.com>
2020-08-25 08:27:26 +0800

    uplode code

--- rd5_assessment/money.php
+++ rd5_assessment/money.php
@@ -10,6 +10,9 @@
     if(isset($_POST["btnWithdrawal"])){
         header("location: Withdrawal.php");
     }
+    if(true){
+        
+    }
 ?>
 
 <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
